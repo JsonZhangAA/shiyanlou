14295599b188fe95c46ef62f106ff57ffb996799
JsonZhangAA <1942143988@qq.com>
2019-03-17 18:05:40 +0800

    use epoll to build a chat room

---
+++ epoll/utility.h
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <list>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+/**********************   macro defintion **************************/
+// server ip
+#define SERVER_IP "127.0.0.1"
+
+// server port
+#define SERVER_PORT 8888
+
+//epoll size
+#define EPOLL_SIZE 5000
+
+//message buffer size
+#define BUF_SIZE 0xFFFF
+
+#define SERVER_WELCOME "Welcome you join  to the chat room! Your chat ID is: Client #%d"
+
+#define SERVER_MESSAGE "ClientID %d say >> %s"
+
+// exit
+#define EXIT "EXIT"
+
+#define CAUTION "There is only one int the char room!"
+class MUsers{
+public:
+	int chatId;
+	int nums;
+	bool operator==(const MUsers u){
+		return u.nums==nums;
+	}
+	bool operator!=(const MUsers & u){
+		return u.nums!=nums;
+	}
+	MUsers(int _nums):nums(_nums){}
+};
+list<MUsers> clients_list;
+
+int setnonblocking(int sockfd){
+	fcntl(sockfd,F_SETFL,fcntl(sockfd,F_GETFD,0)|O_NONBLOCK);
+	return 0;
+}
+
+void addfd(int epollfd,int fd,bool enable_et){
+	struct epoll_event ev;
+	ev.data.fd=fd;
+	ev.events=EPOLLIN;
+	if(enable_et)
+		ev.events=EPOLLIN|EPOLLET;
+	epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+	setnonblocking(fd);
+	printf("fd added to epoll!\n\n");
+}
+
+int sendBroadcastmessage(int clientfd){
+	char buf[BUF_SIZE],message[BUF_SIZE];
+	bzero(buf,BUF_SIZE);
+	bzero(message,BUF_SIZE);
+	printf("read from client(clientID=%d)\n",clientfd);
+	int len=recv(clientfd,buf,BUF_SIZE,0);
+	if(len==0){
+		close(clientfd);
+		MUsers * u=new MUsers(clientfd);
+		list<MUsers>::iterator it;
+		it=find(clients_list.begin(),clients_list.end(),*u);
+		if(it!=clients_list.end()){
+			cout<<"remove "<<it->nums<<endl;
+			clients_list.remove(*it);
+		}
+		printf("ClientID=%d closed.\n now there are %d client in the char room\n",clientfd,(int)clients_list.size());
+	}else{
+		if(clients_list.size()==1){
+			send(clientfd,CAUTION,strlen(CAUTION),0);
+			return len;
+		}
+		list<MUsers>::iterator it;
+		int MchatId;
+		MUsers  * u=new MUsers(clientfd);
+		/*for(it=clients_list.begin();it!=clients_list.end();++it){
+			if(it->nums==clientfd){
+				MchatId=it->chatId;
+			}
+		}*/
+		it=find(clients_list.begin(),clients_list.end(),*u);
+		if(it!=clients_list.end()){
+			MchatId=it->chatId;
+		}
+		sprintf(message,SERVER_MESSAGE,clientfd,buf);
+		for(it=clients_list.begin();it!=clients_list.end();++it){
+			if(it->nums!=clientfd&&it->chatId==MchatId){
+				if(send(it->nums,message,BUF_SIZE,0)<0){
+					perror("error");
+					exit(-1);
+				}
+			}
+		}
+	}
+	return len;	
+}
